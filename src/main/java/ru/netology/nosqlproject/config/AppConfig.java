a052899941061a953601c48b8a5ba3f0bd48bb3d
Dmitriy9417 <89084940+Dmitriy9417@users.noreply.github.com>
2025-07-21 21:40:45 +0300

    Delete src/main/java/ru/netology/nosqlproject/config directory
    delete Appconfig.java

--- src/main/java/ru/netology/nosqlproject/config/AppConfig.java
+++
@@ -1,9 +0,0 @@
-package ru.netology.nosqlproject.config;
-
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@ComponentScan("ru.netology.nosqlproject")
-public class AppConfig {
-}
